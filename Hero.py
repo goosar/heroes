75420001a227903d4227bc1312adbc3b31bc59a9
Arne Goos <Arne.Goos@gmail.com>
2018-06-14 00:42:37 +0200

    initial comit

---
+++ Hero.py
@@ -0,0 +1,11 @@
+#Hero.py
+# Copyright Arne Goos 2018
+
+class Character():
+    initiativeBase = 6
+
+  
+
+class Hero(Character):
+    pass
+
---
+++ run.py
@@ -0,0 +1,6 @@
+#run.py
+# Copyright Arne Goos 2018
+import Hero
+
+a = Hero.Hero()
+print(a.initiativeBase)
